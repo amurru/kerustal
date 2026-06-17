0c56f78d8019a89b191b5bf1c1df9b5632e70759
Amurru Zerouk <me@amurru.dev>
2026-06-17 12:56:21 +0300

    basic keyboard input

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 use bootloader::entry_point;
 use core::panic::PanicInfo;
+use x86_64::instructions::port::Port;
 
 // Import out new vga_buffer module
 mod vga_buffer;
@@ -10,9 +11,30 @@ mod vga_buffer;
 entry_point!(kernel_main);
 
 fn kernel_main(_boot_info: &'static bootloader::BootInfo) -> ! {
-    println!("Hi Cosmos from The Macro World!");
-    println!("Are you {} coder? So what is {}?", 1337, 42);
-    loop {}
+    println!("KeRustal OS Initialized!");
+    println!("Type something inside the QEMU window...");
+
+    // Create ports mapped to the keyboard controller registers
+    let mut status_port = Port::<u8>::new(0x64);
+    let mut data_port = Port::<u8>::new(0x60);
+    loop {
+        unsafe {
+            // Read the status register
+            let status = status_port.read();
+
+            // Bit 0 of the status register is set to 1 if there is data waiting
+            if status & 1 == 1 {
+                // Read the actual scancode from the data register
+                let scancode = data_port.read();
+
+                // For now, let's just print the raw hardware number!
+                // NOTE: Scancodes below 0x80 mean a key was pressed, above 0x80 means released.
+                if scancode < 0x80 {
+                    print!("[Key:{:#X}]", scancode);
+                }
+            }
+        }
+    }
 }
 
 #[panic_handler]
